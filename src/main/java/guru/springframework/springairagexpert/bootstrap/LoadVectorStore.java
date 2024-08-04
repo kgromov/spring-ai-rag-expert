69f44de2ed1d135ab35421f6701592ad6c952c73
konstantin <konst.gromov@gmail.com>
2024-08-05 02:17:23 +0300

    Update with 1.0.0-M1 milistone; add support for multiple vectore store data source

--- src/main/java/guru/springframework/springairagexpert/bootstrap/LoadVectorStore.java
+++ src/main/java/guru/springframework/springairagexpert/bootstrap/LoadVectorStore.java
@@ -1,6 +1,9 @@
 package guru.springframework.springairagexpert.bootstrap;
 
 import guru.springframework.springairagexpert.config.VectorStoreProperties;
+import io.milvus.client.MilvusClient;
+import io.milvus.grpc.ListDatabasesResponse;
+import io.milvus.param.R;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.ai.document.Document;
--- src/main/java/guru/springframework/springairagexpert/config/VectorStoreConfig.java
+++ src/main/java/guru/springframework/springairagexpert/config/VectorStoreConfig.java
@@ -3,13 +3,11 @@ package guru.springframework.springairagexpert.config;
 import io.milvus.client.MilvusServiceClient;
 import org.springframework.ai.embedding.EmbeddingModel;
 import org.springframework.ai.vectorstore.MilvusVectorStore;
-import org.springframework.ai.vectorstore.MongoDBAtlasVectorStore;
 import org.springframework.ai.vectorstore.VectorStore;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
-import org.springframework.data.mongodb.core.MongoTemplate;
 
 @Configuration
 public class VectorStoreConfig {
@@ -19,12 +17,5 @@ public class VectorStoreConfig {
     @Bean
     public VectorStore milvusVectorStore(MilvusServiceClient milvusClient, EmbeddingModel embeddingModel) {
         return new MilvusVectorStore(milvusClient, embeddingModel, true);
-    }
-
-    @Profile("mongodb")
-    @Qualifier("vectorStore")
-    @Bean
-    public VectorStore mongodbVectorStore(MongoTemplate mongoTemplate, EmbeddingModel embeddingModel) {
-        return new MongoDBAtlasVectorStore(mongoTemplate, embeddingModel, true);
     }
 }
